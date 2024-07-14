f5ba363295dfe4698fbb3696e6156a1fccb03b38
Dustin Whisman <whisman.dustin@gmail.com>
2024-07-14 09:57:29 -0600

    feat: use IIFE to allow script to run multiple times without errors

--- ana11yze.js
+++ ana11yze.js
@@ -1,32 +1,34 @@
-const getDeepestDirectDiv = (selector = 'div > div', depth = 2, lastElement = null) => {
-  const element = document.querySelector(selector);
-  if (!element) {
-    return {
-      element: lastElement,
-      selector,
-      depth,
-    };
-  }
+(() => {
+	const getDeepestDirectDiv = (selector = 'div > div', depth = 2, lastElement = null) => {
+		const element = document.querySelector(selector);
+		if (!element) {
+			return {
+				element: lastElement,
+				selector,
+				depth,
+			};
+		}
 
-  return getDeepestDirectDiv(selector + ' > div', depth + 1, element);
-};
+		return getDeepestDirectDiv(selector + ' > div', depth + 1, element);
+	};
 
-const getDeepestDiv = (selector = 'div div', depth = 2, lastElement = null) => {
-  const element = document.querySelector(selector);
-  if (!element) {
-    return {
-      element: lastElement,
-      selector,
-      depth,
-    };
-  }
+	const getDeepestDiv = (selector = 'div div', depth = 2, lastElement = null) => {
+		const element = document.querySelector(selector);
+		if (!element) {
+			return {
+				element: lastElement,
+				selector,
+				depth,
+			};
+		}
 
-  return getDeepestDiv(selector + ' div', depth + 1, element);
-};
+		return getDeepestDiv(selector + ' div', depth + 1, element);
+	};
 
-const checkSiteForIssues = () => {
-  console.log('Deepest direct div:', getDeepestDirectDiv());
-  console.log('Deepest div:', getDeepestDiv());
-};
+	const checkSiteForIssues = () => {
+		console.log('Deepest direct div:', getDeepestDirectDiv());
+		console.log('Deepest div:', getDeepestDiv());
+	};
 
-checkSiteForIssues();
+	checkSiteForIssues();
+})();
