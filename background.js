dd4db5762ef2fcc5f42591b868a488c56f2fc8d3
Dustin Whisman <whisman.dustin@gmail.com>
2024-07-14 10:00:40 -0600

    feat: set up script to enable toggling extension on/off

---
+++ background.js
@@ -0,0 +1,70 @@
+let scriptsEnabled = false;
+
+const PAGE_ACTION = globalThis.browser ? 'pageAction' : 'action';
+globalThis.browser = globalThis.browser ?? chrome;
+
+async function registerContentScripts() {
+	await browser.scripting.registerContentScripts([
+		{
+			id: 'ana11yze.js',
+			js: ['ana11yze.js'],
+			matches: ['*://*/*'],
+		},
+		{
+			id: 'visua11yze.css',
+			css: ['visua11yze.css'],
+			matches: ['*://*/*'],
+		},
+	]);
+}
+
+async function toggleContentScripts(tab) {
+	const scripts = await browser.scripting.getRegisteredContentScripts({
+		ids: [
+			'ana11yze.js',
+			'visua11yze.css',
+		],
+	});
+
+	if (scriptsEnabled) {
+		scripts.forEach((script) => {
+			if (script.css) {
+				browser.scripting.removeCSS({
+					files: script.css,
+					target: {
+						tabId: tab.id
+					}
+				});
+				return;
+			}
+		});
+	} else {
+		scripts.forEach((script) => {
+			if (script.js) {
+				browser.scripting.executeScript({
+					files: script.js,
+					target: {
+						tabId: tab.id
+					}
+				});
+				return;
+			}
+
+			if (script.css) {
+				browser.scripting.insertCSS({
+					files: script.css,
+					target: {
+						tabId: tab.id
+					}
+				});
+				return;
+			}
+		});
+	}
+
+	scriptsEnabled = !scriptsEnabled;
+}
+
+browser[PAGE_ACTION].onClicked.addListener(toggleContentScripts);
+
+registerContentScripts();
