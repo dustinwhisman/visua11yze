4b0bd7717af956dadce2a1a90ff8f16285a558fc
dustinwhisman <whisman.dustin@gmail.com>
2024-08-02 15:20:59 -0600

    feat: add checks for headings in the wrong order

--- rules/1-3-adaptable/checks.js
+++ rules/1-3-adaptable/checks.js
@@ -1,10 +1,102 @@
 (() => {
-	const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+	console.log({ headings });
 	const levelOneHeadings = [];
+	let previousHeadingLevel;
 	for (const heading of headings) {
-		if (heading.tagName === 'H1') {
-			levelOneHeadings.push(heading);
+		switch (heading.tagName) {
+			case 'H1':
+				levelOneHeadings.push(heading);
+
+				if (previousHeadingLevel != null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+				}
+				break;
+			case 'H2':
+				if (previousHeadingLevel == null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-one', '');
+				}
+				break;
+			case 'H3':
+				if (previousHeadingLevel == null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-one', '');
+				}
+
+				if (previousHeadingLevel === 'H1') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-two', '');
+				}
+				break;
+			case 'H4':
+				if (previousHeadingLevel == null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-one', '');
+				}
+
+				if (previousHeadingLevel === 'H1') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-two', '');
+				}
+
+				if (previousHeadingLevel === 'H2') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-three', '');
+				}
+				break;
+			case 'H5':
+				if (previousHeadingLevel == null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-one', '');
+				}
+
+				if (previousHeadingLevel === 'H1') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-two', '');
+				}
+
+				if (previousHeadingLevel === 'H2') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-three', '');
+				}
+
+				if (previousHeadingLevel === 'H3') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-four', '');
+				}
+				break;
+			case 'H6':
+				if (previousHeadingLevel == null) {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-one', '');
+				}
+
+				if (previousHeadingLevel === 'H1') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-two', '');
+				}
+
+				if (previousHeadingLevel === 'H2') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-three', '');
+				}
+
+				if (previousHeadingLevel === 'H3') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-four', '');
+				}
+
+				if (previousHeadingLevel === 'H4') {
+					heading.setAttribute('data-heading-out-of-order', '');
+					heading.setAttribute('data-skipped-level-five', '');
+				}
+				break;
+			default:
+				break;
 		}
+
+		previousHeadingLevel = heading.tagName;
 	}
 
 	if (levelOneHeadings.length < 1) {
